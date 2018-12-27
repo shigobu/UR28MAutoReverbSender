1870819ae7e868a7f417a586c3bda4a830a50220
sh-kasamatsu <sh-kasamatsu@apptec.co.jp>
2018-12-27 10:07:20 +0900

    ラジオボタンのイベント追加

--- UR28MAutoReverbSender/MainWindow.xaml.cs
+++ UR28MAutoReverbSender/MainWindow.xaml.cs
@@ -367,6 +367,24 @@ namespace UR28MAutoReverbSender
 				}
 			}
 		}
+
+		/// <summary>
+		/// ノート・CCラジオボタンチェック変更のイベントハンドラ
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void radio_Checked(object sender, RoutedEventArgs e)
+		{
+			RadioButton rb = sender as RadioButton;
+			if (rb.Equals(noteRadio))
+			{
+				noteNum.IsEnabled = rb.IsChecked ?? noteNum.IsEnabled;
+			}
+			else if (rb.Equals(ccRadio))
+			{
+				ccNum.IsEnabled = rb.IsChecked ?? ccNum.IsEnabled;
+			}
+		}
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
